e2baa7a753a9b2682593b1d200f49062d39660f2
alphacalcu <alphacalcu@163.com>
2019-07-22 09:04:33 +0800

    Create __init__.py

---
+++ Analysis/TechnicalAnalysis/__init__.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+from Analysis.DataProviders import DataProvider
+from Analysis.SecurityValueHolders import SecurityShiftedValueHolder
+from Analysis.SecurityValueHolders import SecurityDeltaValueHolder
+from Analysis.SecurityValueHolders import SecurityIIFValueHolder
+from Analysis.SecurityValueHolders import SecurityConstArrayValueHolder
+from Analysis.CrossSectionValueHolders import CSRankedSecurityValueHolder
+from Analysis.CrossSectionValueHolders import CSTopNSecurityValueHolder
+from Analysis.CrossSectionValueHolders import CSBottomNSecurityValueHolder
+from Analysis.CrossSectionValueHolders import CSAverageSecurityValueHolder
+from Analysis.CrossSectionValueHolders import CSAverageAdjustedSecurityValueHolder
+from Analysis.CrossSectionValueHolders import CSZScoreSecurityValueHolder
+from Analysis.CrossSectionValueHolders import CSFillNASecurityValueHolder
+from Analysis.CrossSectionValueHolders import CSPercentileSecurityValueHolder
+from Analysis.CrossSectionValueHolders import CSResidueSecurityValueHolder
+from Analysis.SecurityValueHolders import SecurityCurrentValueHolder
+from Analysis.SecurityValueHolders import SecurityLatestValueHolder
+from Analysis import TechnicalAnalysis
+from Analysis.transformer import transform
+
+__all__ = ['DataProvider',
+           'SecurityShiftedValueHolder',
+           'SecurityDeltaValueHolder',
+           'SecurityIIFValueHolder',
+           'SecurityConstArrayValueHolder',
+           'CSRankedSecurityValueHolder',
+           'CSTopNSecurityValueHolder',
+           'CSBottomNSecurityValueHolder',
+           'CSAverageSecurityValueHolder',
+           'CSAverageAdjustedSecurityValueHolder',
+           'CSZScoreSecurityValueHolder',
+           'CSFillNASecurityValueHolder',
+           'CSPercentileSecurityValueHolder',
+           'CSResidueSecurityValueHolder',
+           'SecurityCurrentValueHolder',
+           'SecurityLatestValueHolder',
+           'TechnicalAnalysis',
+           'transform']
